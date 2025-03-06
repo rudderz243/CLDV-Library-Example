4aecd1fa0b149944fbd7e181b11902db34d6a1a3
Glynn <79942619+rudderz243@users.noreply.github.com>
2025-03-06 20:09:04 +0200

    added EF example, completed SQL example

---
+++ LibraryManagerEF/Book.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagerEF
+{
+    // we created a class that exactly matches the table in the database
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public double ReplacementPrice { get; set; }
+        public int CategoryID { get; set; }
+    }
+
+    // then create a DbContext class, in order to use EF to interact with out data
+    // we inherit from DbContext, the main class of EF
+    public class AppDbContext : DbContext
+    {
+        // we create a "set" of all the data in the book table, and make it accessible 
+        public DbSet<Book> Books { get; set; }
+        // we then override a default method, in order to pass through our connection string
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=lab7L95SR\\SQLEXPRESS; Initial Catalog=LibraryDB; Integrated Security=True; TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ LibraryManagerEF/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace LibraryManagerEF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static void AddBook()
+        {
+            // get user input
+            Console.WriteLine("Book Title:");
+            string title = Console.ReadLine();
+            Console.WriteLine("Book Price:");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("CategoryID:");
+            int cat_id = Convert.ToInt32(Console.ReadLine());
+
+            // create new instance of our database, using EF
+            var db_context = new AppDbContext();
+            // use that instance we created to interact with the DB
+            using (db_context)
+            {
+                // create a new Book using the class we created and the input we got.
+                var new_book = new Book
+                {
+                    Title = title,
+                    ReplacementPrice = price,
+                    CategoryID = cat_id
+                };
+                // add the book to the database
+                db_context.Add(new_book);
+                // save changes
+                db_context.SaveChanges();
+
+                Console.WriteLine("Added successfully!");
+            }
+        }
+    }
+}
